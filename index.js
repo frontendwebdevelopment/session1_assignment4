fd830f782a09390f633641f252fbeb2e7b819ea9
Ajay Kumar S M <ajaymandakki@gmail.com>
2016-06-26 11:10:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+function name(name) {
+  function printname() {       
+    console.log("Name: " + name);  
+  }
+  function printage(age) {       
+    console.log("Age: " + age);  
+  }
+  function printcity(city) {
+  	console.log("City: " + city)
+  }
+  printname();
+  printage(23);
+  printcity("Bengaluru");
+}
+name ("Ajay");
